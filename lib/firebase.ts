bd381427808eb7a912d80ed540c315e5cfe92114
benjacostm100 <168737592+benjacostm100@users.noreply.github.com>
2026-06-29 13:04:41 -0300

    peformance4

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps } from "firebase/app"
 import { getAuth, connectAuthEmulator } from "firebase/auth"
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore"
+import { initializeFirestore, connectFirestoreEmulator, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions"
 
@@ -17,7 +17,11 @@ const firebaseConfig = {
 export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
 
 export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentMultipleTabManager(),
+  }),
+})
 export const storage = getStorage(app)
 export const functions = getFunctions(app)
 
